1a847c3bf832f8d598cae2418c6d82f8da161422
ADEBZ <YOUR_EMAIL>
2024-03-24 12:21:24 +0100

    attempted update using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,39 @@ import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 
 function App() {
-  
+
 
   return (
     <><div className={styles.App}>
-       <Navbar/>
-       <Hero/>
-       <About/>
-       <Experience/>
-       <Projects/>
-       <Contact/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
     </div>
-     
+
     </>
   )
 }
 
 export default App;
+
+
+/**
+ * import React, { lazy } from 'react';
+import { BrowserRouter as Route,  Routes, Router } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={React.createElement(lazy(() => import("../src/pages/Home")))} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
+
+ */
