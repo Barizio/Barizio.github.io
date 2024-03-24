c5931f4360f6faf912d3853cdda7ddb43d4ad50f
ADEBZ <YOUR_EMAIL>
2024-03-24 12:48:28 +0100

    added _blank property to my links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,17 +11,17 @@ export const Contact = () => {
     <ul className={styles.links}>
         <li className={styles.link}>
         <img src={getiImageUrl("contact/emailIcon.png")} alt="emailicon" />
-        <a href="mailto:adesokanadebare@gmail.com">adebareadesokan@gmail.com</a>
+        <a href="mailto:adesokanadebare@gmail.com" target='_blank'>adebareadesokan@gmail.com</a>
         </li>
 
         <li className={styles.link}>
         <img src={getiImageUrl("contact/linkedinIcon.png")} alt="linkedinicon" />
-        <a href="www.linkedin.com/in/adebare-adesokan-570b7b260">linkedin.com/Adebare Adesokan</a>
+        <a href="www.linkedin.com/in/adebare-adesokan-570b7b260" target='_blank'>linkedin.com/Adebare Adesokan</a>
         </li>
 
         <li className={styles.link}> 
         <img src={getiImageUrl("contact/githubIcon.png")} alt="githubicon" />
-        <a href="https://github.com/Barizio">github.com/Adebare-Adesokan</a>
+        <a href="https://github.com/Barizio" target='_blank'>github.com/Adebare-Adesokan</a>
         </li>
     </ul>
   </footer>
