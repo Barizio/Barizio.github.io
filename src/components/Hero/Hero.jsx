d3759c3fb0e025b496df30b7045a6ea4f4064e90
ADEBZ <YOUR_EMAIL>
2024-03-24 12:20:48 +0100

    fixed hero bug

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { getiImageUrl } from '../../utils';
 import styles from "./Hero.module.css";
 export const Hero = () => {
-  return
-  <section className={styles.container}>
-    <div className={styles.content}>
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
         <h1 className={styles.title}>
-            Hi, I'm Adebare
+          Hi, I'm Adebare
         </h1>
         <p className={styles.description}>
-            i do stuff i will put in later
+          i do stuff i will put in later
         </p>
-        <a className={styles.contact.Btn } href="mailto:adesokanadebare@gmail.com">Contact Me</a>
-    </div>
-    <img src={getiImageUrl("hero/heroImage.png")} alt="Hero Image of me" className={styles.heroImg}/>
-    <div className={styles.topBlur}></div>
-    <div className={styles.bottomBlur}></div>
-  </section>
-  ;
+        <a className={styles.contactBtn} href="mailto:adesokanadebare@gmail.com">Contact Me</a>
+      </div>
+      <img src={getiImageUrl("hero/heroImage.png")} alt="Hero Image of me" className={styles.heroImg} />
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
+    </section>
+  )
 }
